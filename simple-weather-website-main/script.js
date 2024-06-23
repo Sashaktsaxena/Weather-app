8c4427737f7d39a2747870d8c7609119ec50d6e0
Sashaktsaxena <118295782+Sashaktsaxena@users.noreply.github.com>
2024-06-23 22:04:07 +0530

    Update script.js

--- simple-weather-website-main/script.js
+++ simple-weather-website-main/script.js
@@ -1,5 +1,5 @@
 let weather = {
-    "apikey": "659121eff80fc8f4a34b91c5f056bf8e",
+    "apikey": "YOUR_API_KEY",
     fetchWeather : function (city) {
         fetch("https://api.openweathermap.org/data/2.5/weather?q=" 
         + city 
